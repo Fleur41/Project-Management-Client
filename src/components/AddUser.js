33ee4d8cd97efedd95aebf8012a610b5e64a411f
Fleur41 <Muriithikings456@gmail.com>
2023-03-05 22:50:18 +0300

    AddUser.js

---
+++ src/components/AddUser.js
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button'
+import AddUserForm from "./AddUserForm";
+
+export default function AddUser({onAddNewUser, onCancelAddUser}){
+
+    const [addUser, setAddUser] = useState(false)
+
+
+    function onCancelAddUser() {
+    setAddUser(false)
+  }
+
+  
+
+  //if add user is false, show add user button, if add user is true, show add user form
+    return(
+        <div>
+            
+            {!addUser ? 
+           
+            <div className="d-grid gap-2">
+            <Button 
+            className='add-something-button'
+            variant="primary" 
+            size="lg"
+            onClick={()=>setAddUser((prev)=>!prev)}>
+            Add New Team Member
+            </Button>
+            </div>
+
+
+
+            : 
+            <AddUserForm 
+            onCancelAddUser={onCancelAddUser}
+            onAddNewUser={onAddNewUser}
+            setAddUser={setAddUser}
+            /> }
+        </div>
+    )
+}
