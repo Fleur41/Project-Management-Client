c79321ecc0a0194ae13547e4cfdcae6dd1fbacf4
Fleur41 <Muriithikings456@gmail.com>
2023-03-06 13:46:36 +0300

    src/components/Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,16 +2,9 @@ import React, {useState, useEffect} from "react";
 import HomeCarousel from "./HomeCarousel";
 
 export default function Home(){
-   const [users, setUsers] = useState([])
-  useEffect(()=>{
-  fetch(`http://localhost:9292/projects`)
-  .then(res => res.json())
-  .then(users => setUsers(users))
-  }, [])
-  console.log(users);
     return(
-        <h1>
+        <div style={{background:"#242842"}}>
             <HomeCarousel/>
-        </h1>
+        </div>
     )
 }
