521c388a25d928e32dae1887c36ba1f479e0d547
Fleur41 <Muriithikings456@gmail.com>
2023-03-06 13:47:17 +0300

    src/components/HomeCarousel.js

--- src/components/HomeCarousel.js
+++ src/components/HomeCarousel.js
@@ -1,18 +1,21 @@
-import Carousel from "react-bootstrap/Carousel";
-import React from "react";
-import "../App.css";
+import Carousel from 'react-bootstrap/Carousel';
+import React from 'react';
+import  '../App.css';
+
+
 
 export default function HomeCarousel() {
   return (
-    <Carousel id="Carousel">
+    <Carousel id='Carousel' style={{margin:"auto", width:"80%"}}>
       <Carousel.Item>
         <img
           className="d-block w-100"
           src="https://www.triaxtec.com/wp-content/uploads/2018/02/shutterstock_178613660.jpg"
           alt="First slide"
+          height= "500px"
         />
-        <Carousel.Caption style={{ backgroundColor: "white", color: "black" }}>
-          <h3>Welcome to Project Land</h3>
+        <Carousel.Caption style={{border:"2px solid", margin:"10px",background:"gray"}}>
+          <h3>Welcome to Strategize.</h3>
           <p>Add Yourself to the TEAM MEMBERS Page</p>
           <p>To Get Started.</p>
         </Carousel.Caption>
@@ -22,9 +25,11 @@ export default function HomeCarousel() {
           className="d-block w-100"
           src="https://www.simplilearn.com/ice9/free_resources_article_thumb/PM.jpg"
           alt="Second slide"
+          height= "500px"
+          
         />
 
-        <Carousel.Caption>
+        <Carousel.Caption style={{border:"2px solid", margin:"10px",background:"gray"}}>
           <h3>Create a Project</h3>
           <p>View our Projects Page to Manage Projects.</p>
         </Carousel.Caption>
@@ -34,22 +39,15 @@ export default function HomeCarousel() {
           className="e-block w-100"
           src="https://cloudinary.hbs.edu/hbsit/image/upload/s--gAAnShcM--/f_auto,c_fill,h_375,w_750,/v20200101/63FA86712AEE497FD9F306C31133994E.jpg"
           alt="Third slide"
+          height= "500px"
         />
 
-        <Carousel.Caption
-          style={{
-            backgroundColor: "white",
-            color: "black",
-            alignSelf: "flex-start",
-            display: "flex",
-          }}
-        >
-          <h3>
-            See Completed Projects. Navigate to COMPLETED PROJECTS to see what
-            we've already done!
-          </h3>
+        <Carousel.Caption style={{border:"2px solid", margin:"10px",background:"gray"}}>
+          <h3>See Completed Projects. Navigate to COMPLETED PROJECTS to see what we've already done!</h3>
+          
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
 }
+
