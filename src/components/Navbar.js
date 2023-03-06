511e9f0fef16a6fbb2481e04969904c4149e96c7
Fleur41 <Muriithikings456@gmail.com>
2023-03-06 13:50:30 +0300

    src/components/Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, NavLink, Switch } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
+import './Navbar.css'
 
 
 export default function NavBar({currentUser, handleLogout}) {
@@ -9,33 +10,42 @@ export default function NavBar({currentUser, handleLogout}) {
     return (
 
       <>
-
-      <Nav fill variant="tabs" >
-      <Nav.Item>
-        <Nav.Link href="/" eventKey="/">HOME</Nav.Link>
+      <Nav fill variant="tabs" style={{display:"flex", margin:"20px" , height:"70px", alignItems:"center"}} >
+      <Nav.Item className="Nav--item">
+        <Nav.Link className="Nav--link" href="/" eventKey="/">HOME</Nav.Link>
       </Nav.Item>
 
-      <Nav.Item>
-        <Nav.Link  href="/users" eventKey="/users" >TEAM MEMBERS
+      <Nav.Item className="Nav--item">
+        <Nav.Link className="Nav--link" href="/users" eventKey="/users" >TEAM MEMBERS
         </Nav.Link>
       </Nav.Item>
 
-      <Nav.Item>
-        <Nav.Link href='/projects' eventKey="/projects">PROJECTS</Nav.Link>
+      <Nav.Item className="Nav--item">
+        <Nav.Link className="Nav--link" href='/projects' eventKey="/projects">PROJECTS</Nav.Link>
       </Nav.Item>
 
-      <Nav.Item>
-        <Nav.Link href='/archives' eventKey="/archives">COMPLETED PROJECTS</Nav.Link>
+      <Nav.Item className="Nav--item">
+        <Nav.Link className="Nav--link" href='/archives' eventKey="/archives">COMPLETED PROJECTS</Nav.Link>
       </Nav.Item>
 
-      <Nav.Item>
+      
+      <Nav.Item className="Nav--item">
+        <div className="right--items login" >
         {currentUser ? 
-        <>Logged in as {currentUser.name}
-        <button onClick={handleLogout}>logout</button></>
+        <>
+        <button className="logout" onClick={handleLogout}>LOGOUT</button>
+        <div className="login--text" style={{marginBottom:"40px", color:"green"}}>Logged in as {currentUser.name}</div>
+        </>
         :
-        <Nav.Link href='/login' eventKey="/login">LOGIN</Nav.Link>
+        <Nav.Link className="Nav--link" href='/login' eventKey="/login">LOGIN</Nav.Link>
         }
+        </div>
       </Nav.Item>
+      <div className="right--items registration">
+      <Nav.Item className="Nav--item">
+        <Nav.Link className="Nav--link" href='/register' eventKey="/register">REGISTER</Nav.Link>
+      </Nav.Item>
+      </div>
     </Nav>
 
 
