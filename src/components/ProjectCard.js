1cd5f81dd6faafd1cbfcdceb9594aa2537e30904
Fleur41 <Muriithikings456@gmail.com>
2023-03-06 13:51:46 +0300

    src/components/ProjectCard.js

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -16,6 +16,7 @@ export default function ProjectCard({
     fetch(`http://localhost:9292/projects/${id}`, { method: "DELETE" });
     onClickDeleteButton(id);
   }
+  
 
   function handlePatch(obj) {
     const objForPatch = {
@@ -25,7 +26,7 @@ export default function ProjectCard({
       statusChangeOn: obj.statusChangeOn,
     };
 
-    fetch(`http://localhost:9292/rojects/${obj.id}`, {
+    fetch(`http://localhost:9292/projects/${obj.id}`, {
       method: "PATCH",
       headers: { "Content-type": "application/json; charset=UTF-8" },
       body: JSON.stringify(objForPatch),
@@ -42,7 +43,7 @@ export default function ProjectCard({
   }
 
   return (
-    <div>
+    <div className="project-card" style={{border:"2px solid white", marginBottom:"10px", background:"gray" ,width:"50%"}}>
       {!edit ? (
         <DisplayProjectCard
           handleDelete={handleDelete}
