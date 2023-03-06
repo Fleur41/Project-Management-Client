bb1fa14425b366a7b25e6c8ab0e0bc918a787269
Fleur41 <Muriithikings456@gmail.com>
2023-03-06 13:43:51 +0300

    src/components/AddUser.js

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,38 +1,43 @@
-import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import AddProjectForm from "./AddProjectForm";
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button'
 import AddUserForm from "./AddUserForm";
 
-export default function AddUser({ onAddNewUser, onCancelAddUser }) {
-  // add user or add member
-  const [addUser, setAddUser] = useState(false);
+export default function AddUser({onAddNewUser, onCancelAddUser}){
 
-  const onCancelAddUser = () => {
-    setAddUser(false);
+    const [addUser, setAddUser] = useState(false)
+
+
+    function onCancelAddUser() {
+    setAddUser(false)
   }
 
+  
+
   //if add user is false, show add user button, if add user is true, show add user form
-  return (
-    <div>
-      {!addUser ? (
-        <div className="d-grid gap-2">
-          <Button
-            className="add-something-button"
-            variant="primary"
+    return(
+        <div>
+            
+            {!addUser ? 
+           
+            <div className="d-grid gap-2">
+            <Button 
+            className='Add-user'
+            variant="primary" 
             size="lg"
-            onClick={() => setAddUser((prev) => !prev)}
-          >
-            Add New Team Member
-          </Button>
+            onClick={()=>setAddUser((prev)=>!prev)}
+            >
+            Add New Team Member &#43;
+            </Button>
+            </div>
+
+
+
+            : 
+            <AddUserForm 
+            onCancelAddUser={onCancelAddUser}
+            onAddNewUser={onAddNewUser}
+            setAddUser={setAddUser}
+            /> }
         </div>
-      ) : (
-        <AddUserForm
-          onCancelAddUser={onCancelAddUser}
-          onAddNewUser={onAddNewUser}
-          setAddUser={setAddUser}
-        />
-      )}
-    </div>
-  );
+    )
 }
